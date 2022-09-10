1d8b6e016a841a1e87ee3aea6e85248af370a3a1
“yuhan” <yuhanyao9@gmail.com>
2022-09-10 10:28:23 -0400

    work01

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,7 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,6 +100,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i  >= 0; i--) {
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -143,6 +147,9 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        for (String s : to_split.split(" ")) {
+            ret.append(s.charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,11 +177,15 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        for (int i = 0; i < arr.length; i++) {
+            current_sum += i % 2 == 0 ? 0 : arr[i];
+        }
 
         return current_sum;
     }
 
 
+    /*
     /*
      * TODO (Task 6): Submit the changes you made on GitHub!
      *                When you submit it, go to the 'Actions' tab. You should
